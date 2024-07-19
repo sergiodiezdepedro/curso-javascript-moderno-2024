4e5aecf210148ffbfe2a3fe74aa08d91cfc73097
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2024-07-19 12:41:45 +0200

    wip: Basics

---
+++ 01-fundamentos/app.js
@@ -0,0 +1,32 @@
+// console.log('Hola Mundo desde un archivo JS');
+
+// var b = 11
+// const c = 12
+
+// let a = 10
+// let b = 11;
+// let c = 11;
+// let d = 11;
+// let x = a + b;
+
+let a = 10,
+  b = 50,
+  c = 10,
+  d = 10,
+  x = a + b,
+  e = "Hola ",
+  f = "Spiderman";
+
+const saludo = e + f;
+
+
+// console.info(x);
+// console.error(x);
+// console.warn(x);
+// console.log(`El resultado de a + b es ${x}`)
+// console.log({x})
+
+c = "Hola de nuevo";
+
+// console.table({ a, b, c, d, e, f, x });
+// console.log({saludo});
--- notebook.md
+++ notebook.md
@@ -1,2 +1,7 @@
 # Curso de JavaScript Moderno 2024
 
+## Depurar en Visual Studio Code
+
+Desde el archivo que se quiere depurar => Menu => Ejecutar => Iniciar depuración (también con el atajo de teclado F5).
+
+En el menú de opciones seleccionar Node.js y se abre la depuración muy similar a la de Chrome.
