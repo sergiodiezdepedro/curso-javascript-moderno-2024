25276fd3b4b682e15ddb78a007e47ec8140bbca5
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2024-08-02 08:57:55 +0200

    Añadido vite.config.js para facilitar despliegue de la app Blackjack en GitHub Pages

---
+++ 03-blackjack-vite/vite.config.js
@@ -0,0 +1,7 @@
+import {defineConfig} from 'vite';
+
+export default defineConfig({
+    base: "/lorem-ipsum/"
+})
+
+// * 'lorem-ipsum' es el trozo final de la url del proyecto en GitHub. Ejemplo: https://sergiodiezdepedro.github.io/react-curso2024-gifsapp/, sería entonces: /react-curso2024-gifsapp/
